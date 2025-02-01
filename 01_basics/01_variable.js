421821f0156048c2b2bdd9cf264794e65bfe140a
Aniket1019 <aniketprasad1719@gmail.com>
2025-02-01 14:51:15 +0530

    Basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 2547738
+let accountEmial = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"; //not preferd but can exist
+let accountState;
+
+// accountId = 2  // not allowed
+
+
+accountEmial = "hhdd@google.com"
+accountPassword = "4737303"
+accountCity = "BLR"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmial, accountPassword, accountCity, accountState])
